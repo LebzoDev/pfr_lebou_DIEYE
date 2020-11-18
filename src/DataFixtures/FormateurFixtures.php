6b85978b4de159fb157d54ebfb38e38fb8af0640
lebzodev <leboundiayee123@gmai.com>
2020-11-18 23:01:35 +0000

    Heritage faite

--- src/DataFixtures/FormateurFixtures.php
+++ src/DataFixtures/FormateurFixtures.php
@@ -2,15 +2,43 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Profil;
+use App\Entity\Apprenant;
+use App\Repository\ProfilRepository;
+use App\DataFixtures\F1ProfilFixtures;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class FormateurFixtures extends Fixture
 {
+    private $encoder;
+    private $repoProfil;
+    public function __construct(UserPasswordEncoderInterface $encoder, ProfilRepository $repoProfil){
+        $this->repoProfil = $repoProfil;
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create("fr_FR");
+        
+        for ($i=1; $i <=10 ; $i++) { 
+            $user = new Apprenant();
+            $password =$this->encoder->encodePassword($user,"passer");
+            $profil = new Profil();
+
+            $user->setProfil($this->getReference(F1ProfilFixtures::PROFIL_FORMATEUR_REFERENCE));
+            
+            $user->setPrenom($faker->firstName)
+                 ->setNom($faker->lastName)
+                 ->setUsername($faker->userName)
+                 ->setPassword($password);
+            
+            $manager->persist($user);
+
+        }
 
         $manager->flush();
     }
